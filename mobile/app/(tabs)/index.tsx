5c5e1b66e915b4a7e3921ee68512d952f81b3bb2
zhqriks <zhorikmartanov228@gmail.com>
2023-12-19 18:21:04 +0600

    add main page

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,90 +1,102 @@
-import { Github, Twitter } from "@tamagui/lucide-icons";
-import { Link, useRouter } from "expo-router";
 import {
-  Button,
-  H1,
-  ListItem,
-  Paragraph,
-  Separator,
-  YGroup,
-  YStack
+    Card, CardProps,
+    H2,
+    ScrollView,
+    XStack,
+    YStack,
+    Image, H3, Paragraph, Input, H4,
 } from "tamagui";
 
 import { MyStack } from "../../components/MyStack";
 
+export const EventCard = ({title, description} : {title: string, description: string }) => {
+    return (
+        <Card elevate bordered size="$4" height={100} backgroundColor='transparent'>
+            <Card.Header padded>
+                <XStack justifyContent="space-between">
+                    <H4>{title}</H4>
+                    <H4>9am - 6pm</H4>
+                </XStack>
+                <XStack justifyContent="space-between">
+                    <Paragraph>{description}</Paragraph>
+                    <Paragraph>Dec 16</Paragraph>
+                </XStack>
+            </Card.Header>
+        </Card>
+    )
+};
+export const ServiceCard = ({title, description, imageUrl} : {title: string, description: string, imageUrl: string}) => {
+    return (
+        <YStack >
+            <Image
+                source={{ uri: imageUrl }}
+                style={{ width: 160, height: 140}}
+                borderRadius={8}
+            />
+            <YStack>
+                <H3>{title}</H3>
+                <Paragraph>{description}</Paragraph>
+            </YStack>
+        </YStack>
+
+    )
+}
+
+export const ScienceOrganizationCard = ({title, description, imageUrl, ...props}: CardProps & {title: string, description: string, imageUrl: string}) => {
+  return (
+      <Card elevate bordered size="$4" {...props} width={150} height={250}>
+        <Card.Header padded style={{backgroundColor: 'rgba(0,0,0,0.2)'}}>
+          <H3 color='white'>{title}</H3>
+          <Paragraph color='white'>{description}</Paragraph>
+        </Card.Header>
+        <Card.Background>
+            <Image
+                source={{ uri: imageUrl }}
+                style={{ width: '100%', height: '100%' }}
+                borderRadius={8}
+            />
+        </Card.Background>
+      </Card>
+  )
+}
+
 export default function Home() {
-  const router = useRouter();
 
   return (
     <MyStack>
-      <YStack
-        space="$4"
-        maxWidth={600}
-      >
-        <H1 textAlign="center">Добро пожаловать в PhysTech.</H1>
-        <Paragraph textAlign="center">
-          Here&apos;s a basic starter to show navigating from one screen to
-          another.
-        </Paragraph>
-      </YStack>
+        <ScrollView>
+            <YStack space="$3"  >
+                  <YStack
+                      space="$2.5"
+                  >
+                      <Input size="$4" placeholder="Search" borderRadius='$space.10'  />
+                      <H2>Научные Организаций</H2>
+                    <ScrollView horizontal space="$5" mt="$space.2">
+                      <ScienceOrganizationCard title="PhysTech" description="Description of the org" imageUrl="https://media.istockphoto.com/id/1270632735/photo/model-of-atom-and-elementary-particles-physics-concept-3d-rendered-illustration.jpg?s=612x612&w=0&k=20&c=n-Y_YPN6MtUS6OZf74oW2Bn68snhi6J2WI5ua3w0Skg=" />
+                      <ScienceOrganizationCard title="BioTech" description="Description of the org" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeavdP9LMP2O_5PlsdSSCOM3nbt0fsLjPNVA&usqp=CAU" />
+                    </ScrollView>
+                  </YStack>
 
-      <YStack space="$2.5">
-        <Button onPress={() => router.push("/users/testuser")}>
-          Go to user page
-        </Button>
-        <Button onPress={() => router.push("/tabs")}>Go to tabs page</Button>
-      </YStack>
+                  <YStack>
+                      <H2>Уcлуги</H2>
+                      <ScrollView horizontal space="$5" mt="$space.2">
+                        <ServiceCard title="Service" description="Description of Service" imageUrl="https://www.enterpriseappstoday.com/wp-content/uploads/2023/05/Clinical-Laboratory-Services-market.jpg" />
+                        <ServiceCard title="Service" description="Description of Service" imageUrl="https://www.enterpriseappstoday.com/wp-content/uploads/2023/05/Clinical-Laboratory-Services-market.jpg" />
+                        <ServiceCard title="Service" description="Description of Service" imageUrl="https://www.enterpriseappstoday.com/wp-content/uploads/2023/05/Clinical-Laboratory-Services-market.jpg" />
 
-      <YStack space="$5">
-        <YGroup
-          bordered
-          separator={<Separator />}
-          theme="green"
-        >
-          <YGroup.Item>
-            <Link
-              asChild
-              href="https://twitter.com/natebirdman"
-              target="_blank"
-            >
-              <ListItem
-                hoverTheme
-                title="Nate"
-                pressTheme
-                icon={Twitter}
-              />
-            </Link>
-          </YGroup.Item>
-          <YGroup.Item>
-            <Link
-              asChild
-              href="https://github.com/tamagui/tamagui"
-              target="_blank"
-            >
-              <ListItem
-                hoverTheme
-                pressTheme
-                title="Tamagui"
-                icon={Github}
-              />
-            </Link>
-          </YGroup.Item>
-          <YGroup.Item>
-            <Link
-              asChild
-              href="https://github.com/ivopr/tamagui-expo"
-              target="_blank"
-            >
-              <ListItem
-                hoverTheme
-                pressTheme
-                title="This Template"
-                icon={Github}
-              />
-            </Link>
-          </YGroup.Item>
-        </YGroup>
-      </YStack>
+                      </ScrollView>
+                  </YStack>
+
+                  <YStack>
+                    <H2>Мероприятия</H2>
+                    <ScrollView space="$3" mt="$space.2">
+                        <EventCard title="Service" description="Description of Service" />
+                        <EventCard title="Service" description="Description of Service" />
+                        <EventCard title="Service" description="Description of Service" />
+                    </ScrollView>
+                  </YStack>
+            </YStack>
+        </ScrollView>
     </MyStack>
   );
 }
