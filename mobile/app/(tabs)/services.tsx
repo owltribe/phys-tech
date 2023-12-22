60a8900383bb5d711e53c3d5be4c3a91ef28068a
Jarasar <49629027+ZhQriks@users.noreply.github.com>
2023-12-23 00:07:49 +0600

    add services page (#5)
    * add services page
    
    * fix cards align
    
    * fix cards space

--- mobile/app/(tabs)/services.tsx
+++ mobile/app/(tabs)/services.tsx
@@ -1,16 +1,52 @@
-import { useRouter } from "expo-router";
 import {
-    Paragraph,
+    H2,
+    H3,
+    Image, Input,
+    Paragraph, ScrollView, Stack, YStack,
 } from "tamagui";
 
 import { MyStack } from "../../components/MyStack";
 
+export const ServiceCard = ({title, description, imageUrl} : {title: string, description: string, imageUrl: string}) => {
+    return (
+        <YStack>
+            <Image
+                source={{ uri: imageUrl }}
+                style={{ width: 180, height: 140}}
+                borderRadius={8}
+            />
+            <YStack>
+                <H3>{title}</H3>
+                <Paragraph>{description}</Paragraph>
+            </YStack>
+        </YStack>
+
+    )
+};
+
 export default function Services() {
-    const router = useRouter();
 
     return (
         <MyStack>
-            <Paragraph>Hello world IT IS Services</Paragraph>
+                <YStack space="$3">
+                    <YStack
+                        space="$2.5"
+                    >
+                        <H2>Услуги</H2>
+                        <Input size="$4" placeholder="Искать Услуги" borderRadius='$space.10'  />
+                        <ScrollView mt="$space.2">
+                            <Stack flexDirection='row' flexWrap="wrap" justifyContent="space-between" space="$space.2">
+                                <ServiceCard title="Service" description="Description of Service" imageUrl="https://www.enterpriseappstoday.com/wp-content/uploads/2023/05/Clinical-Laboratory-Services-market.jpg" />
+                                <ServiceCard title="Service" description="Description of Service" imageUrl="https://www.enterpriseappstoday.com/wp-content/uploads/2023/05/Clinical-Laboratory-Services-market.jpg" />
+                                <ServiceCard title="Service" description="Description of Service" imageUrl="https://www.enterpriseappstoday.com/wp-content/uploads/2023/05/Clinical-Laboratory-Services-market.jpg" />
+                                <ServiceCard title="Service" description="Description of Service" imageUrl="https://www.enterpriseappstoday.com/wp-content/uploads/2023/05/Clinical-Laboratory-Services-market.jpg" />
+                                <ServiceCard title="Service" description="Description of Service" imageUrl="https://www.enterpriseappstoday.com/wp-content/uploads/2023/05/Clinical-Laboratory-Services-market.jpg" />
+                                <ServiceCard title="Service" description="Description of Service" imageUrl="https://www.enterpriseappstoday.com/wp-content/uploads/2023/05/Clinical-Laboratory-Services-market.jpg" />
+                            </Stack>
+                        </ScrollView>
+                    </YStack>
+
+                </YStack>
         </MyStack>
     );
 }
