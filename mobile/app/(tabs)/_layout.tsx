76e0951163c06d9d5f46c8013cec0cbde9f90e15
AsgardPavlov <pasgard9@gmail.com>
2023-12-25 21:11:55 +0600

    fix bottom nav bar styles

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,48 +1,93 @@
-import { Tabs, useRouter } from "expo-router";
-import {Home, Search, TableProperties, UserCircle} from "@tamagui/lucide-icons";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import {
+  Home,
+  Search,
+  TableProperties,
+  UserCircle
+} from "@tamagui/lucide-icons";
+import { color } from "@tamagui/themes";
+import { Tabs } from "expo-router";
+import { white } from "utils/colors";
 
+function getTabIcon(routeName, focused: boolean) {
+  const color = focused ? "$blue9" : "$gray8";
+  const icons = {
+    index: (
+      <Home
+        color={color}
+        size="$2"
+        strokeWidth={1.7}
+      />
+    ),
+    services: (
+      <TableProperties
+        color={color}
+        size="$2"
+        strokeWidth={1.7}
+      />
+    ),
+    search: (
+      <Search
+        color={color}
+        size="$2"
+        strokeWidth={1.7}
+      />
+    ),
+    profile: (
+      <UserCircle
+        color={color}
+        size="$2"
+        strokeWidth={1.7}
+      />
+    )
+  };
 
-const Color = {
-    Active: '#eb4c60',
-    Disabled: '#afafaf',
-};
-
-function getTabIcon(routeName, focused) {
-    const icons = {
-        index: <Home color={focused ? Color.Active : Color.Disabled} />,
-        services: <TableProperties color={focused ? Color.Active : Color.Disabled} />,
-        search: <Search color={focused ? Color.Active : Color.Disabled} />,
-        profile: <UserCircle color={focused ? Color.Active : Color.Disabled} />
-    };
-
-    return icons[routeName] || null;
+  return icons[routeName] || null;
 }
 
 export default function Layout() {
-  const router = useRouter();
+  const insets = useSafeAreaInsets();
 
   return (
-      <Tabs
-          screenOptions={({ route }) => ({
-              headerShown: false,
-              tabBarIcon: ({ focused }) => getTabIcon(route.name, focused),
-              tabBarActiveTintColor: '#787878',
-              tabBarInactiveTintColor: 'gray',
-              tabBarStyle: { backgroundColor: '#fff' },
-          })}
-      >
-        <Tabs.Screen name='index' options={{
-            tabBarLabel: 'Home',
-        }}  />
-        <Tabs.Screen name='services' options={{
-            tabBarLabel: 'Services',
-        }} />
-        <Tabs.Screen name='search' options={{
-            tabBarLabel: 'Search',
-        }} />
-        <Tabs.Screen name='profile' options={{
-            tabBarLabel: 'Profile',
-        }} />
-      </Tabs>
+    <Tabs
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused }) => getTabIcon(route.name, focused),
+        tabBarActiveTintColor: color.gray9Light.toString(),
+        tabBarInactiveTintColor: color.gray9Light.toString(),
+        tabBarStyle: {
+          backgroundColor: white,
+          height: 68 + insets.bottom,
+          paddingVertical: 10,
+          rowGap: 8
+        },
+        tabBarLabelStyle: { fontSize: 11, fontWeight: "700" }
+      })}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Главная"
+        }}
+      />
+      <Tabs.Screen
+        name="services"
+        options={{
+          tabBarLabel: "Услуги"
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarLabel: "Поиск"
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Профиль"
+        }}
+      />
+    </Tabs>
   );
 }
