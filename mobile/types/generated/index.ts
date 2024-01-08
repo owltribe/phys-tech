2fe07d309d62e07c0e60972ef2f9351bc5997fbc
AsgardPavlov <pasgard9@gmail.com>
2024-01-08 18:19:35 +0600

    add generated types

--- mobile/types/generated/index.ts
+++ mobile/types/generated/index.ts
@@ -7,6 +7,7 @@ export type { Body_auth_jwt_login_auth_login_post } from './models/Body_auth_jwt
 export type { ErrorModel } from './models/ErrorModel';
 export type { HTTPValidationError } from './models/HTTPValidationError';
 export type { UserCreate } from './models/UserCreate';
+export type { UserProfile } from './models/UserProfile';
 export type { UserRead } from './models/UserRead';
 export type { UserRoleEnum } from './models/UserRoleEnum';
 export type { ValidationError } from './models/ValidationError';
