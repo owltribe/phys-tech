e8f62eada9dea02607905a60c9d8a5afe81243b0
AsgardPavlov <pasgard9@gmail.com>
2024-01-09 18:29:48 +0600

    add generated types

--- mobile/types/generated/index.ts
+++ mobile/types/generated/index.ts
@@ -3,6 +3,7 @@
 /* tslint:disable */
 /* eslint-disable */
 
+export type { BearerResponse } from './models/BearerResponse';
 export type { Body_auth_jwt_login_auth_login_post } from './models/Body_auth_jwt_login_auth_login_post';
 export type { ErrorModel } from './models/ErrorModel';
 export type { HTTPValidationError } from './models/HTTPValidationError';
