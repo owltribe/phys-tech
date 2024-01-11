e4ad835115548e713057abdfd343405db75c40fe
AsgardPavlov <pasgard9@gmail.com>
2024-01-11 18:48:40 +0600

    add hooks

---
+++ mobile/hooks/auth/useLogin.ts
@@ -0,0 +1,30 @@
+import { useMutation, UseMutationResult } from "@tanstack/react-query";
+import { AxiosError, AxiosResponse } from "axios";
+import {
+  BearerResponse,
+  Body_auth_jwt_login_auth_login_post,
+  ErrorModel
+} from "types/generated";
+import axiosInstance from "utils/axios-instance";
+
+export default function useLogin(): UseMutationResult<
+  AxiosResponse<BearerResponse>,
+  AxiosError<ErrorModel>,
+  Body_auth_jwt_login_auth_login_post
+> {
+  const login = (payload: Body_auth_jwt_login_auth_login_post) => {
+    const formData = new FormData();
+    formData.append("username", payload.username);
+    formData.append("password", payload.password);
+
+    return axiosInstance.post("/auth/login", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    });
+  };
+
+  return useMutation({
+    mutationFn: login
+  });
+}
