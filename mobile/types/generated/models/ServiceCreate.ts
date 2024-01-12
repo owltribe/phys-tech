691288907a889a26e1091dc976e4f355532c0b2d
AsgardPavlov <pasgard9@gmail.com>
2024-01-12 19:28:23 +0600

    add service generated types

---
+++ mobile/types/generated/models/ServiceCreate.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type ServiceCreate = {
+    name: string;
+    description: (string | null);
+    result: (string | null);
+    cost: number;
+};
+
