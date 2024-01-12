949850c8cbbcc7a9982df6306b4477456bd475df
AsgardPavlov <pasgard9@gmail.com>
2024-01-12 19:22:33 +0600

    add db sync session

--- server/database.py
+++ server/database.py
@@ -1,16 +1,23 @@
 from datetime import datetime
 from typing import AsyncGenerator
 
-from sqlalchemy import TIMESTAMP
+from sqlalchemy import TIMESTAMP, create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy.orm import DeclarativeBase, mapped_column, sessionmaker
 
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+ASYNC_SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
+async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL)
+
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False)
+
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, future=True
+)
 
 
 class Base(DeclarativeBase):
@@ -20,8 +27,17 @@ class Base(DeclarativeBase):
     pass
 
 
+# DB Utilities
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 async def create_db_and_tables():
-    async with engine.begin() as conn:
+    async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 
