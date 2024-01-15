f005f8f1c455b17b9d19ca30a6fd49a62dadfa8b
AsgardPavlov <pasgard9@gmail.com>
2024-01-15 22:51:14 +0600

    fix organization model and add migration

--- server/models/organization.py
+++ server/models/organization.py
@@ -1,14 +1,16 @@
 import uuid
 from enum import Enum
-from sqlalchemy import String, Integer, Column, Enum as SQLEnum
+from sqlalchemy import String, Enum as SQLEnum, UUID
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
+
 
 class Category(Enum):
     scientific_organization = "Научная организация"
     university = "Вуз"
     technopark = "Технопарк"
     commercial_laboratory_company = "Коммерческая Лабораторная компания"
+
 
 class Organization(Base):
     __tablename__ = "organization"
