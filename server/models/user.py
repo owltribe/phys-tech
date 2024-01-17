5a29fdeb5e4685f460c86a55d8f9d3f1b1cc8489
AsgardPavlov <pasgard9@gmail.com>
2024-01-17 18:40:45 +0600

    add service requests and relations

--- server/models/user.py
+++ server/models/user.py
@@ -5,7 +5,7 @@ from pydantic import computed_field
 from sqlalchemy import String, Boolean, Enum, UUID
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 from fastapi_users_db_sqlalchemy import UUID_ID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
 
@@ -15,14 +15,11 @@ class UserRole(enum.Enum):
     Client = "Client"
 
 
-UserRoleEnum: Enum = Enum(UserRole, name="user_role_enum")
-
-
 class User(SQLAlchemyBaseUserTableUUID, Base):
     __tablename__ = "user"
 
     id: Mapped[UUID_ID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
-    role = mapped_column(UserRoleEnum, nullable=False)
+    role = mapped_column(Enum(UserRole, name="user_role_enum"), nullable=False)
     email: Mapped[str] = mapped_column(
         String(length=320), unique=True, index=True, nullable=False
     )
@@ -37,6 +34,14 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
     )
     is_verified: Mapped[bool] = mapped_column(
         Boolean, default=False, nullable=False
+    )
+
+    requested_services = relationship(
+        "ServiceRequest",
+        back_populates="requested_by",
+        primaryjoin="User.id == ServiceRequest.requested_by_id",
+        cascade="delete",
+        uselist=True
     )
 
     @computed_field
