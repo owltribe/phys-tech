9b3d0aa33e13f464a2f5e756628061f2e1724b5e
AsgardPavlov <pasgard9@gmail.com>
2024-01-29 18:05:14 +0600

    add privacy-policy and terms-and-conditions

--- server/main.py
+++ server/main.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from fastapi_pagination import add_pagination
 from sqladmin import Admin
 
 from database import engine
-from models.user import User
 from src.auth.admin import UserAdmin, admin_authentication_backend
-from src.auth.auth_backend import current_active_user
 from src.auth.router import auth_router
+from src.event.router import events_router
 from src.organization.admin import OrganizationAdmin
+from src.organization.router import organizations_router
 from src.service.admin import ServiceAdmin
 from src.service.router import services_router
-from src.organization.router import organizations_router
-from src.event.router import events_router
 from src.service_request.admin import ServiceRequestAdmin
 from src.service_request.router import service_request_router
 
@@ -23,8 +23,9 @@ admin = Admin(
     app,
     engine,
     authentication_backend=admin_authentication_backend,
-    title="PhysTech Admin"
+    title="PhysTech Admin",
 )
+templates = Jinja2Templates(directory="templates")
 
 # Admin
 admin.add_view(UserAdmin)
@@ -40,14 +41,15 @@ app.include_router(events_router)
 app.include_router(service_request_router)
 
 
-# Protected router example
-@app.get("/protected-route")
-def protected_route(user: User = Depends(current_active_user)):
-    return f"Hello, {user.email}"
+@app.get("/privacy-policy", response_class=HTMLResponse)
+def privacy_policy(request: Request):
+    return templates.TemplateResponse(
+        name="privacy-policy.html", context={"request": request}
+    )
 
 
-@app.get("/unprotected-route")
-def protected_route():
-    return {
-        "data": f"Hello, World"
-    }
+@app.get("/terms-and-conditions", response_class=HTMLResponse)
+async def terms_and_conditions(request: Request):
+    return templates.TemplateResponse(
+        name="terms-and-conditions.html", context={"request": request}
+    )
