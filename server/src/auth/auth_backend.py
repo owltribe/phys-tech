a69fc4092296b56e472024ecbc9a1a79e1388faa
AsgardPavlov <pasgard9@gmail.com>
2024-02-01 16:25:13 +0600

    add PermissionChecker

--- server/src/auth/auth_backend.py
+++ server/src/auth/auth_backend.py
@@ -15,7 +15,8 @@ bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=AUTH_SECRET, lifetime_seconds=3600)
+    # lifetime_seconds is 12 hours
+    return JWTStrategy(secret=AUTH_SECRET, lifetime_seconds=60 * 60 * 12)
 
 
 auth_backend = AuthenticationBackend(
--- server/src/service/service.py
+++ server/src/service/service.py
@@ -21,25 +21,25 @@ class ServiceService:
         self.organization_service = OrganizationService(session)
         self.service_request_service = ServiceRequestService(session)
 
-    def get_services(self, service_filter: ServiceFilter) -> Page[ServiceRead]:
+    def paginated_list(self, service_filter: ServiceFilter) -> Page[ServiceRead]:
         query = select(Service)
         query = service_filter.filter(query)
         query = service_filter.sort(query)
 
         return paginate(self.session, query)
 
-    def get_services_for_user_requests(self, current_user: User) -> Page[ServiceRead]:
-        requested_services = self.service_request_service.get_services_for_user(current_user)
+    def user_requested_services_paginated_list(self, current_user: User) -> Page[ServiceRead]:
+        requested_services = self.service_request_service.get_users_service_requests(current_user)
         requested_service_ids = [req_service.service.id for req_service in requested_services]
 
         query = select(Service).filter(Service.id.in_(requested_service_ids))
 
         return paginate(self.session, query)
 
-    def get_service(self, service_id: str):
+    def retrieve(self, service_id: str) -> Service | None:
         return self.session.query(Service).filter(Service.id == service_id).first()
 
-    def create_service(self, service: ServiceCreate, current_user: User):
+    def create(self, service: ServiceCreate, current_user: User):
         organization = self.organization_service.get_organization_by_user_id(current_user.id)
 
         if organization is None:
@@ -60,23 +60,19 @@ class ServiceService:
         self.session.refresh(db_service)
         return db_service
 
-    def update_service(self, service_id: str, updated_service: ServiceUpdate):
-        db_service = self.session.query(Service).filter(Service.id == service_id).first()
-        if db_service:
-            for key, value in updated_service.dict().items():
-                setattr(db_service, key, value)
-            self.session.commit()
-            self.session.refresh(db_service)
-        return db_service
+    def update(self, service_id: str, updated_service: ServiceUpdate):
+        instance = self.retrieve(service_id)
 
-    def delete_service(self, service_id: str):
+        if instance:
+            for key, value in updated_service.dict().items():
+                setattr(instance, key, value)
+            self.session.commit()
+            self.session.refresh(instance)
+        return instance
+
+    def destroy(self, service_id: str):
         db_service = self.session.query(Service).filter(Service.id == service_id).first()
         if db_service:
             self.session.delete(db_service)
             self.session.commit()
         return db_service
-
-
-# TODO uncomment when organization entity relation is added
-# def get_services_by_organization_id(db: Session, organization_id: str):
-#     return db.query(Service).filter(Service.organization_id == organization_id)
