18e5b973685db62d6e8f7ae5f862f3904a677089
AsgardPavlov <pasgard9@gmail.com>
2024-02-22 17:23:42 +0600

    add fonts loading

--- mobile-v2/App.tsx
+++ mobile-v2/App.tsx
@@ -1,12 +1,13 @@
 import "./src/components/sheets";
 
+import { useCallback } from "react";
 import { LogBox, StatusBar } from "react-native";
 import { SheetProvider } from "react-native-actions-sheet";
 import { PaperProvider } from "react-native-paper";
 import { registerTranslation, ru } from "react-native-paper-dates";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useFonts } from "expo-font";
-// import * as SplashScreen from "expo-splash-screen";
+import * as SplashScreen from "expo-splash-screen";
 import { AuthProvider } from "providers/AuthProvider";
 import { ReactQueryClientProvider } from "providers/ReactQueryProvider";
 
@@ -22,11 +23,11 @@ export default function App() {
     "GoogleSans-Bold": require("./assets/fonts/GoogleSans-Bold.ttf")
   });
 
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded || fontError) {
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [fontsLoaded, fontError]);
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
 
   if (!fontsLoaded && !fontError) {
     return null;
@@ -37,6 +38,7 @@ export default function App() {
       <AuthProvider>
         <SafeAreaProvider
           style={{ flex: 1, paddingTop: StatusBar.currentHeight }}
+          onLayout={onLayoutRootView}
         >
           <SheetProvider>
             <PaperProvider theme={theme}>
