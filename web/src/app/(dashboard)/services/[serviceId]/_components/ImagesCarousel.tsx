c2c4ebde7ec3181e1f39991f4063676bf9a245e8
AsgardPavlov <pasgard9@gmail.com>
2024-04-16 00:48:53 +0700

    web: add service images slider

---
+++ web/src/app/(dashboard)/services/[serviceId]/_components/ImagesCarousel.tsx
@@ -0,0 +1,45 @@
+'use client'
+import '@splidejs/react-splide/css';
+
+// @ts-ignore
+import {Splide, SplideSlide, SplideTrack} from "@splidejs/react-splide";
+
+import { ServiceImageRead } from "@/types/generated";
+
+interface ImageCarouselProps {
+  images: ServiceImageRead[]
+}
+
+const ImagesCarousel = ({
+  images
+}: ImageCarouselProps) => {
+  return (
+    <div className="relative flex h-full w-full">
+      <Splide
+        id="services-images"
+        aria-label="Service Images"
+        hasTrack={images.length > 1}
+        options={{
+          autoplay: true,
+          interval: 2000,
+          arrows: images.length > 1
+        }}
+        height="100%"
+        className="h-full w-full"
+      >
+        <SplideTrack />
+        {images.map(i => (
+          <SplideSlide key={i.id}>
+            <img
+              src={i.url}
+              alt={`image-${i.id}`}
+              className="h-full w-full object-cover"
+            />
+          </SplideSlide>
+        ))}
+      </Splide>
+    </div>
+  )
+}
+
+export default ImagesCarousel
--- web/src/app/(dashboard)/services/[serviceId]/page.tsx
+++ web/src/app/(dashboard)/services/[serviceId]/page.tsx
@@ -1,11 +1,11 @@
 import { Metadata} from "next";
 import {fetchService} from "@/hooks/services/useService";
-import {Badge, Card, DataList, Flex, Heading, Link, Text} from "@radix-ui/themes";
-import Image from "next/image";
+import {Card, DataList, Heading, Link, Text, Theme} from "@radix-ui/themes";
 import {Instagram} from "lucide-react";
 
 import ActionsCard from "./_components/ActionsCard";
 import {formatPrice} from "@/lib/formatters";
+import ImagesCarousel from "@/app/(dashboard)/services/[serviceId]/_components/ImagesCarousel";
 
 interface ServiceDetailProps {
   params: {
@@ -30,70 +30,67 @@ export default async function ServiceDetail({
   const service = await fetchService(serviceId)
 
   return (
-    <div className="p-6 space-y-4 container mx-auto">
-      {service && (
-        <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
-          <div className="order-1 col-span-1 lg:col-span-3 flex flex-col space-y-6">
-            <Card size="3" className="aspect-video overflow-hidden">
-              <div className="absolute inset-y-0 inset-x-0 w-full">
-                {!!service.service_images?.length ? (
-                  <Image
-                    fill
-                    className="object-cover"
-                    alt={service.service_images[0].id}
-                    src={service.service_images[0].url}
-                  />
-                ) : (
-                  <div className="bg-gray-100 flex w-full h-full justify-center items-center">
-                    <Instagram className="h-10 w-10" />
-                  </div>
-                )}
-              </div>
-            </Card>
+    <Theme radius="small" accentColor="blue">
+      <div className="p-6 space-y-4 container mx-auto">
+        {service && (
+          <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
+            <div className="order-1 col-span-1 lg:col-span-3 flex flex-col space-y-6">
+              <Card size="3" className="aspect-video overflow-hidden">
+                <div className="absolute inset-y-0 inset-x-0 w-full">
+                  {!!service.service_images?.length ? (
+                    <ImagesCarousel images={service.service_images} />
+                  ) : (
+                    <div className="bg-gray-100 flex w-full h-full justify-center items-center">
+                      <Instagram className="h-10 w-10" />
+                    </div>
+                  )}
+                </div>
+              </Card>
 
-            <Card size="3" >
-              <Heading size="6">
-                {service.name}
-              </Heading>
-              <Text mt="2" as="div" color="gray" size="2">
-                {service.description}
-              </Text>
+              <Card size="3" >
+                <Heading size="6">
+                  {service.name}
+                </Heading>
+                <Text mt="2" as="div" color="gray" size="2">
+                  {service.description}
+                </Text>
 
-              <DataList.Root mt="6" trim="start">
-                <DataList.Item>
-                  <DataList.Label minWidth="88px">Ожидаемый результат</DataList.Label>
-                  <DataList.Value>
-                    {service.expected_result}
-                  </DataList.Value>
-                </DataList.Item>
-                <DataList.Item>
-                  <DataList.Label minWidth="88px">Цена</DataList.Label>
-                  <DataList.Value>{formatPrice(service.cost)}</DataList.Value>
-                </DataList.Item>
-                <DataList.Item align="center">
-                  <DataList.Label minWidth="88px">Организация</DataList.Label>
-                  <DataList.Value>
-                    <Link
-                      href={`/organizations/${service.organization.id}`}
-                      underline="always"
-                    >
-                      {service.organization.name}
-                    </Link>
-                  </DataList.Value>
-                </DataList.Item>
-                <DataList.Item>
-                  <DataList.Label minWidth="88px">Номер телефона</DataList.Label>
-                  <DataList.Value>{service.organization.contact}</DataList.Value>
-                </DataList.Item>
-              </DataList.Root>
-            </Card>
+                <DataList.Root mt="6" trim="start">
+                  <DataList.Item>
+                    <DataList.Label minWidth="88px">Ожидаемый результат</DataList.Label>
+                    <DataList.Value>
+                      {service.expected_result}
+                    </DataList.Value>
+                  </DataList.Item>
+                  <DataList.Item>
+                    <DataList.Label minWidth="88px">Цена</DataList.Label>
+                    <DataList.Value>{formatPrice(service.cost)}</DataList.Value>
+                  </DataList.Item>
+                  <DataList.Item align="center">
+                    <DataList.Label minWidth="88px">Организация</DataList.Label>
+                    <DataList.Value>
+                      <Link
+                        href={`/organizations/${service.organization.id}`}
+                        underline="always"
+                      >
+                        {service.organization.name}
+                      </Link>
+                    </DataList.Value>
+                  </DataList.Item>
+                  <DataList.Item>
+                    <DataList.Label minWidth="88px">Номер телефона</DataList.Label>
+                    <DataList.Value>{service.organization.contact}</DataList.Value>
+                  </DataList.Item>
+                </DataList.Root>
+              </Card>
+            </div>
+
+            <div className="order-2 lg:col-span-2 flex flex-col space-y-6">
+              <ActionsCard service={service} />
+            </div>
           </div>
-
-          <div className="order-2 lg:col-span-2 flex flex-col space-y-6">
-            <ActionsCard service={service} />
-          </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </Theme>
   )
 }
