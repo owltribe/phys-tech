e39c070f72c21ce7bebce616bad0a393dabe44bd
Jarasar <49629027+ZhQriks@users.noreply.github.com>
2024-04-17 16:02:04 +0500

    web: add events page
    Co-authored-by: AsgardPavlov <pasgard9@gmail.com>

---
+++ web/src/app/(dashboard)/events/_components/event-card.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import {EventRead} from "@/types/generated";
+import {Calendar, Clock, MapPin} from "lucide-react";
+import dayjs from "dayjs";
+
+import "dayjs/locale/ru";
+import {Badge, Flex, Heading, Text} from "@radix-ui/themes";
+import {formatMinutesToHoursMinutes} from "@/lib/formatters";
+
+interface EventCardProps {
+  event: EventRead
+}
+
+const EventCard = ({ event }: EventCardProps) => {
+  const fullStartDate = dayjs(`${event.start_date}T${event.start_time}`)
+    .locale("ru")
+    .format("DD MMMM YYYY HH:MM")
+
+  return (
+    <Link href={`/events/${event.id}`}>
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg h-full">
+        <div className="relative w-full aspect-video overflow-hidden">
+          <div className="bg-gray-100 flex w-full h-full justify-center items-center">
+            <Calendar className="h-7 w-7" />
+          </div>
+        </div>
+
+        <Flex pt="2" p="3" direction="column">
+          <Text color="gray" size="2">
+            {fullStartDate}
+          </Text>
+
+          <Heading size="4" mt="2" className="line-clamp-2">
+            {event.name}
+          </Heading>
+
+          <Text color="gray" size="2" mt="2" className="line-clamp-4">
+            {event.description}
+          </Text>
+
+          <Flex my="3" gap="2">
+            <Badge size="2">
+              <Clock className="h-3 w-3" />
+              {formatMinutesToHoursMinutes(event.duration)}
+            </Badge>
+            <Badge size="2">
+              <MapPin className="h-3 w-3" />
+              {event.location}
+            </Badge>
+          </Flex>
+        </Flex>
+      </div>
+    </Link>
+  )
+}
+
+export default EventCard;
---
+++ web/src/hooks/events/useEvents.ts
@@ -0,0 +1,28 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { ErrorModel, Page_EventRead_ } from "@/types/generated";
+import { axiosInstance } from "@/lib/axios-instances";
+
+export default function useEvents({
+                                             search,
+                                         }: {
+    search?: string | null;
+}): UseQueryResult<
+    Page_EventRead_,
+    AxiosError<ErrorModel>
+> {
+    const queryFn = () => {
+        const params = {} as Record<string, string>;
+
+        if (search) {
+            params.search = search;
+        }
+
+        return axiosInstance.get(`/events`, { params: params });
+    };
+
+    return useQuery({
+        queryKey: ["events", search],
+        queryFn,
+    });
+}
