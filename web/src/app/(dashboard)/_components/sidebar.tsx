0092a98249ac35496577e821962d9d7db469fad0
AsgardPavlov <pasgard9@gmail.com>
2024-04-18 22:47:37 +0700

    web: add sidebar google play link

--- web/src/app/(dashboard)/_components/sidebar.tsx
+++ web/src/app/(dashboard)/_components/sidebar.tsx
@@ -1,11 +1,26 @@
 import { SidebarRoutes } from "./sidebar-routes"
+import Image from "next/image";
+import Link from "next/link";
 
 export const Sidebar = () => {
   return (
-    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+    <div className="min-h-[calc(100vh-80px)] overflow-y-auto border-r flex flex-col bg-white shadow-sm">
       <div className="flex flex-col w-full">
         <SidebarRoutes />
       </div>
+
+      <Link
+        href='https://play.google.com/store/apps/details?id=com.asgard.sciencephysservices'
+        className="hover:shadow-sm transition overflow-hidden border rounded-lg p-4 m-3 mt-auto"
+      >
+        <Image
+          height={40}
+          width={140}
+          alt="logo"
+          src="/google-play.svg"
+          className="fill-gray-800"
+        />
+      </Link>
     </div>
   )
 }
