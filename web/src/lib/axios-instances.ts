4e4b8cbfd53f499b3474e1326c07491bbb7f9cec
AsgardPavlov <pasgard9@gmail.com>
2024-04-22 18:56:14 +0700

    server: temporary don't pass credentials in query

--- web/src/lib/axios-instances.ts
+++ web/src/lib/axios-instances.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = (() => {
   const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || "https://science-services-fastapi-zki4d.ondigitalocean.app",
-    withCredentials: true,
+    // withCredentials: true,
     headers: {
       "Content-Type": "application/json"
     }
