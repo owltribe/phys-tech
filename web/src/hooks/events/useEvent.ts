9c152c3590de6206163a1530bddbae22f582ccd2
zhqriks <zhorikmartanov228@gmail.com>
2024-04-26 01:08:03 +0500

    fix reused fetchEvent

--- web/src/hooks/events/useEvent.ts
+++ web/src/hooks/events/useEvent.ts
@@ -4,16 +4,13 @@ import { ErrorModel, EventRead } from "@/types/generated";
 import { axiosInstance } from "@/lib/axios-instances";
 
 export const fetchEvent = (eventId: string): Promise<EventRead> => {
-  return axiosInstance.get(`/events/${eventId}`,);
+  return axiosInstance.get(`/events/${eventId}`);
 };
 
-export default function useEvent(eventId: string): UseQueryResult<
-    EventRead,
-    AxiosError<ErrorModel>
-> {
-  const queryFn = () => {
-    return axiosInstance.get(`/events/${eventId}`,);
-  };
+export default function useEvent(
+  eventId: string
+): UseQueryResult<EventRead, AxiosError<ErrorModel>> {
+  const queryFn = () => fetchEvent(eventId);
 
   return useQuery({
     queryKey: ["events", eventId],
