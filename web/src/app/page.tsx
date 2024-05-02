f9ceabaab6ffc6f04c513c311d973000264c1b6d
AsgardPavlov <pasgard9@gmail.com>
2024-05-03 01:16:32 +0700

    web: add about section

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,13 @@
-import Hero from "@/components/landing/sections/hero";
 import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/sections/hero";
+import About from "@/components/landing/sections/about";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <About />
     </>
   );
 }
---
+++ web/src/components/landing/sections/about.tsx
@@ -0,0 +1,31 @@
+import Button from "@/components/landing/Button";
+
+const About = () => {
+  return (
+    <div className="py-16">
+      <div className="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-16">
+        <div
+          className="lg:bg-gray-50 lg:p-16 rounded-[4rem] space-y-6 md:flex md:gap-6 justify-center md:space-y-0 lg:items-center">
+          <div className="md:5/12 lg:w-1/2">
+            {/*<img src="images/pie.svg" alt="image" loading="lazy" width="" height="">*/}
+          </div>
+          <div className="md:7/12 lg:w-1/2">
+            <h2 className="text-3xl font-bold text-gray-900 md:text-4xl">
+              Science Услуги
+            </h2>
+            <p className="my-8 text-gray-600">
+              Мобильное приложение, инициированное и разработанное Институтом ФизТех, предназначено для создания
+              динамичной платформы, которая направлена на облегчение прямой связи между научными организациями и
+              пользователями, ищущими научные услуги.
+            </p>
+
+            <Button>
+              Начать исследовать
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default About
