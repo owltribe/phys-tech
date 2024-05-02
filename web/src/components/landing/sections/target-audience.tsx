4f2ba2e81a8fa93744862cd4f745e878e6fa31bf
AsgardPavlov <pasgard9@gmail.com>
2024-05-03 01:53:14 +0700

    web: refactor target-audience section

--- web/src/components/landing/sections/target-audience.tsx
+++ web/src/components/landing/sections/target-audience.tsx
@@ -1,3 +1,20 @@
+
+function Card({title, description, imageSrc }: { title: string, description: string, imageSrc: string }) {
+  return (
+    <div className="group relative bg-white transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+      <div className="relative space-y-8 py-12 p-8">
+        <img src={imageSrc} className="w-12" alt={title}/>
+        <div className="space-y-2">
+          <h5 className="text-xl font-medium text-gray-700 transition group-hover:text-primary">
+            {title}
+          </h5>
+          <p className="text-sm text-gray-600">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function TargetAudience() {
   return (
     <div className="py-16">
@@ -12,38 +29,16 @@ export default function TargetAudience() {
           </p>
         </div>
         <div className="mt-16 grid divide-x divide-y divide-gray-100 overflow-hidden rounded-3xl border border-gray-100 sm:grid-cols-2 lg:divide-y-0">
-          <div
-            className="group relative bg-white transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-            <div className="relative space-y-8 py-12 p-8">
-              <img src="https://cdn-icons-png.flaticon.com/512/4341/4341155.png" className="w-12" width="512"
-                   height="512" alt="burger illustration" />
-
-                <div className="space-y-2">
-                  <h5 className="text-xl font-medium text-gray-700 transition group-hover:text-primary">
-                    Организации
-                  </h5>
-                  <p className="text-sm text-gray-600">
-                    Университеты, научно-исследовательские институты и лаборатории, желающие предоставить свои услуги.
-                  </p>
-                </div>
-            </div>
-          </div>
-          <div
-            className="group relative bg-white transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-            <div className="relative space-y-8 py-12 p-8">
-              <img src="https://cdn-icons-png.flaticon.com/512/4341/4341131.png" className="w-12" width="512"
-                   height="512" alt="burger illustration" />
-
-                <div className="space-y-2">
-                  <h5 className="text-xl font-medium text-gray-700 transition group-hover:text-primary">
-                    Клиенты
-                  </h5>
-                  <p className="text-sm text-gray-600">
-                    Частные лица, предприятия или другие организации, нуждающиеся в научных услугах.
-                  </p>
-                </div>
-            </div>
-          </div>
+          <Card
+            title='Организации'
+            description='Университеты, научно-исследовательские институты и лаборатории, желающие предоставить свои услуги.'
+            imageSrc='https://cdn-icons-png.flaticon.com/512/4341/4341155.png'
+          />
+          <Card
+            title='Клиенты'
+            description='Частные лица, предприятия или другие организации, нуждающиеся в научных услугах.'
+            imageSrc='https://cdn-icons-png.flaticon.com/512/4341/4341131.png'
+          />
         </div>
       </div>
     </div>
