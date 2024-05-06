64e0ecf5132be6c58a20a145547d905c028c2ddb
AsgardPavlov <pasgard9@gmail.com>
2024-05-06 20:42:24 +0700

    web: add viewport metadata

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type {Metadata, Viewport} from "next";
 import localFont from "next/font/local";
 import {Theme} from "@radix-ui/themes";
 
@@ -45,6 +45,13 @@ const googleSans = localFont({
   ],
   variable: '--font-google-sans'
 })
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export const metadata: Metadata = {
   title: "Science Услуги | Научные, научно-исследовательские услуги Казахстан",
